0fe2422c7587f5c6462f5486a8b01de0a24a93b0
73azn <amerbinazim@gmail.com>
2025-10-16 12:55:06 +0300

    ameer was here

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('hello crafters'),
+          child: Text('amer are the best in za west'),
         ),
       ),
     );
